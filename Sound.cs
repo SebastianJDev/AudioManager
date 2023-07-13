2187408b430c73ba5f3e2b956456ed82ad78df01
SebastianDev <104598507+SebastianJDev@users.noreply.github.com>
2023-07-13 13:46:44 -0500

    Update Sound.cs
    Cambios2

--- Sound.cs
+++ Sound.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-[System.Serializable]
 
-public class Sound
+namespace Root
 {
-    public string name;
-    public AudioClip clip;
-    [Range(0f, 10f)]
-    public float volume;
-    [Range(.1f, 3f)]
-    public float pitch;
-    public bool loop;
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+        public float volume;
+        public float delay;
+        public float pitch;
+        public bool loop;
 
-    [HideInInspector]
-    public AudioSource source;
+        [HideInInspector]
+        public AudioSource source;
+    }
 }
