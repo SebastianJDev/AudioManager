bf0c4656853f40febfa44bdbc850b024ad49e17a
SebastianJDev <104598507+SebastianJDev@users.noreply.github.com>
2024-02-08 15:32:01 -0500

    Commit2

--- AudioManagerEditorWindow.cs
+++ AudioManagerEditorWindow.cs
@@ -7,8 +7,8 @@ public class AudioManagerEditorWindow : EditorWindow
 {
     private string defaultAudioManagerName = "DefaultAudioManager";
     private GameObject defaultAudioManager;
-
     private List<Sound> sounds = new List<Sound>();
+
 
     private string newSoundName = "NewSound";
     private AudioClip newSoundClip;
@@ -21,13 +21,35 @@ public class AudioManagerEditorWindow : EditorWindow
     {
         GetWindow<AudioManagerEditorWindow>("Audio Manager");
     }
-   
+
     void OnGUI()
     {
+        if (defaultAudioManager == null)
+        {
+            defaultAudioManager = GameObject.FindGameObjectWithTag("AudioManager");
+        }
+
+        // Si no se encuentra el AudioManager o la lista de sonidos está vacía, inicializa la lista
+        if (defaultAudioManager == null || sounds.Count == 0)
+        {
+            sounds = new List<Sound>();
+        }
+        // Si se encuentra un AudioManager en la escena
+        else if (defaultAudioManager != null)
+        {
+            // Actualiza los datos de la ventana con la información del AudioManager
+            AudioManager audioManager = defaultAudioManager.GetComponent<AudioManager>();
+
+            // Actualiza el nombre del AudioManager en la ventana
+            defaultAudioManagerName = audioManager.gameObject.name;
+
+            // Actualiza la lista de sonidos en la ventana con los sonidos del AudioManager
+            sounds = audioManager.sounds;
+        }
         float windowWidth = EditorGUIUtility.currentViewWidth;
 
-        float imageWidth = windowWidth - 20; // Restar el espacio adicional
-        float imageHeight = 130;
+        float imageWidth = windowWidth;
+        float imageHeight = 140;
 
         Texture2D creatorImage = EditorGUIUtility.Load("Assets/AudioManager/CreatorImage.png") as Texture2D;
         GUILayout.BeginHorizontal();
@@ -37,60 +59,97 @@ public class AudioManagerEditorWindow : EditorWindow
         GUILayout.EndHorizontal();
 
 
-        GUILayout.Label("Create Default AudioManager", EditorStyles.boldLabel);
-        defaultAudioManagerName = EditorGUILayout.TextField("Name", defaultAudioManagerName);
-        if (GUILayout.Button("Create Default AudioManager"))
+        GUILayout.BeginHorizontal(GUI.skin.box);
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("CREATE AUDIO MANAGER", EditorStyles.boldLabel);
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+
+        GUILayout.BeginHorizontal(GUI.skin.box);
+        GUILayout.FlexibleSpace();
+        defaultAudioManagerName = EditorGUILayout.TextField(defaultAudioManagerName, GUILayout.Width(imageWidth/2), GUILayout.Height(40));
+        
+        if (GUILayout.Button("CREATE",GUILayout.Width(imageWidth/3),GUILayout.Height(40)))
         {
             CreateDefaultAudioManager();
         }
-
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
         GUILayout.Space(20);
 
-        GUILayout.Label("Create New Sound", EditorStyles.boldLabel);
-        newSoundName = EditorGUILayout.TextField("Name", newSoundName);
-        newSoundClip = EditorGUILayout.ObjectField("Clip", newSoundClip, typeof(AudioClip), false) as AudioClip;
-        newSoundType = (SoundType)EditorGUILayout.EnumPopup("Type", newSoundType);
-        newSoundVolume = EditorGUILayout.Slider("Volume", newSoundVolume, 0f, 1f);
-        newSoundPlayOnAwake = EditorGUILayout.Toggle("Play On Awake", newSoundPlayOnAwake);
+        GUILayout.BeginHorizontal(GUI.skin.box);
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("CREATE NEW SOUND", EditorStyles.boldLabel);
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
 
-        if (GUILayout.Button("Create New Sound"))
+        GUILayout.BeginHorizontal(GUI.skin.box);
+        GUILayout.FlexibleSpace();
+        newSoundName = EditorGUILayout.TextField(newSoundName, GUILayout.Width(imageWidth / 3));
+        newSoundClip = EditorGUILayout.ObjectField(newSoundClip, typeof(AudioClip), false, GUILayout.Width(imageWidth / 3)) as AudioClip;
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+        GUILayout.BeginHorizontal(GUI.skin.box);
+        GUILayout.FlexibleSpace();
+        newSoundType = (SoundType)EditorGUILayout.EnumPopup(newSoundType, GUILayout.Width(imageWidth / 3));
+        newSoundVolume = EditorGUILayout.Slider(newSoundVolume, 0f, 1f, GUILayout.Width(imageWidth / 3));
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+        GUILayout.BeginHorizontal(GUI.skin.box);
+        GUILayout.FlexibleSpace();
+        newSoundPlayOnAwake = EditorGUILayout.Toggle("Play On Awake", newSoundPlayOnAwake);
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
+        if (GUILayout.Button("Create New Sound",GUILayout.Width(imageWidth),GUILayout.Height(40)))
         {
             CreateNewSound();
         }
 
-
         GUILayout.Space(20);
-
-        GUILayout.Label("Sounds in AudioManager", EditorStyles.boldLabel);
-
+        GUILayout.BeginHorizontal(GUI.skin.box);
+        GUILayout.FlexibleSpace();
+        GUILayout.Label("AUDIOS IN AUDIO MANAGER", EditorStyles.boldLabel);
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
         if (defaultAudioManager != null)
         {
             AudioManager audioManager = defaultAudioManager.GetComponent<AudioManager>();
-            foreach (Sound sound in sounds)
-            {
-                GUILayout.BeginHorizontal();
 
+            List<Sound> soundsCopy = new List<Sound>(sounds); // Create a copy of the sounds list
+            foreach (Sound sound in soundsCopy) // Iterate over the copy
+            {
+                GUILayout.BeginHorizontal(GUI.skin.box);
+                GUILayout.FlexibleSpace();
                 GUILayout.Label(sound.name);
-                string newName = EditorGUILayout.TextField("NewName", sound.name);
+                string newName = EditorGUILayout.TextField(sound.name, GUILayout.Width(imageWidth/3));
                 if (newName != sound.name)
                 {
                     sound.name = newName;
                 }
-                if (GUILayout.Button("Delete"))
+                if (GUILayout.Button("DELETE", GUILayout.Width(imageWidth/3)))
                 {
                     audioManager.RemoveSound(sound);
                 }
-
+                GUILayout.FlexibleSpace();
                 GUILayout.EndHorizontal();
             }
         }
     }
 
-
     void CreateDefaultAudioManager()
     {
+        if (defaultAudioManager != null)
+        {
+            Debug.LogWarning("AudioManager already exists in the scene. Delete it before creating a new one.");
+            return;
+        }
+
         defaultAudioManager = new GameObject(defaultAudioManagerName);
         defaultAudioManager.AddComponent<AudioManager>();
+        defaultAudioManager.tag = "AudioManager";
+
+        // Limpiar la lista de sonidos para evitar excepciones
+        sounds.Clear();
     }
 
     void CreateNewSound()
@@ -101,18 +160,15 @@ public class AudioManagerEditorWindow : EditorWindow
             return;
         }
 
-        // Crear el nuevo sonido
         Sound newSound = new Sound();
         newSound.name = newSoundName;
         newSound.clip = newSoundClip;
         newSound.type = newSoundType;
         newSound.volume = newSoundVolume;
 
-        // Obtener el componente AudioManager
         AudioManager audioManager = defaultAudioManager.GetComponent<AudioManager>();
         if (audioManager != null)
         {
-            // Agregar el nuevo sonido a la lista de sonidos del AudioManager
             audioManager.AddSound(newSound);
             Debug.Log("New sound added to AudioManager: " + newSound.name);
             sounds = audioManager.sounds;
@@ -122,5 +178,5 @@ public class AudioManagerEditorWindow : EditorWindow
             Debug.LogWarning("AudioManager component not found on the default AudioManager.");
         }
     }
-   
+
 }
